6b15a448c073384a0a77657cb92a3c06277a10c3
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-11 12:57:53 +0200

    Merging

--- perudo/ia.pl
+++ perudo/ia.pl
@@ -1,3 +1,29 @@
+use_module(library(pairs)).
+
 iajoue(L, X) :- write(L), write('\n'), read(X).
+
+% compter le nombre d'occurences d'un élément dans une liste
+compter([], _, 0).
+compter([X|T], X, Y):- compter(T, X, Z), Y is 1 + Z.
+compter([X1|T], X, Z):- X1 \= X, compter(T, X, Z).
+% compter le nombre d'occurences pour tous
+compteTous(List, X, C) :-
+    sort(List, L), member(X, L),
+    compter(List, X, C).
+
+desVersTirage(Des, Tirage) :-
+  bagof([X, C], compteTous(Des, X, C), Tirage).
+
+%iaAutiste(joueur(N, Des), mise(Nbr, Val), Coup) :- % N est le nombre TOTAL de dés (joueur IA compris)
+%  length(Des, NombreMesDes),
+%  NombreAutresDes is N - NombreMesDes.
+
+second([_, Y], Y).
+
+main :-
+  desVersTirage([3, 1, 1, 5, 5], Tirage),
+  map_list_to_pairs(second, Tirage, Pairs),
+  keysort(Pairs, TirageTrié),
+  write(TirageTrié).
 
 % vim: ft=prolog et sw=2 sts=2
