83b71697b8f98b0a71df07a06d1193a3ff6ccd1d
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-16 15:29:54 +0200

    Ajout du calcul statistique à partir de la forme binomiale
    TODO
    ----
    
    La stat trouvée est entre 0 et 0.3, à débugger/normaliser entre 0 et 1

--- perudo/ia/utils.pl
+++ perudo/ia/utils.pl
@@ -7,4 +7,13 @@ compter([], _, 0).
 compter([X|T], X, Y):- compter(T, X, Z), Y is 1 + Z.
 compter([X1|T], X, Z):- X1 \= X, compter(T, X, Z).
 
+% coefficient binomial
+coefBinomial(_, 0, 1).
+coefBinomial(N, N, 1).
+coefBinomial(N, P, B) :-
+  N1 is N-1, P1 is P-1,
+  coefBinomial(N1, P1, B1),
+  coefBinomial(N1, P, B2),
+  B is B1 + B2.
+
 % vim: ft=prolog et sw=2 sts=2
