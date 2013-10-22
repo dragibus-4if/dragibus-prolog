edaf34fc284990074cc5c6e1bc5a4a677db7c7a8
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-22 15:54:07 +0200

    Ensemble travaillé, pas fini du tout

--- exercice/ensemble.pl
+++ exercice/ensemble.pl
@@ -1,2 +1,13 @@
-% TODO
+:- [liste].
+
+element(_, []) :- fail.
+element(X, [T|Q]) :-
+  X == T ; element(X, Q).
+
+list2ens([], _).
+list2ens([T|Q], E) :-
+  (var(E) -> Es = [] ; Es = E),
+  (element(T, Es) ; concat([T], Es, E)),
+  list2ens(Q, E).
+
 % vim: ft=prolog et sw=2 sts=2
