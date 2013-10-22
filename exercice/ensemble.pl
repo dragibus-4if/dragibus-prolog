3d90749ad8ffec3051d2f5476b133f59130cb6dd
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-22 19:02:13 +0200

    Ajout du rapport

--- exercice/ensemble.pl
+++ exercice/ensemble.pl
@@ -1,9 +1,7 @@
-% permet de vérifier si un élément fait partie d'une liste
 element(_, []) :- fail.
 element(X, [X|_]).
 element(X, [_|Q]) :- element(X, Q).
 
-% permet de construire un ensemble à partir d'une liste
 list2ens([], V, V).
 list2ens([T|Q], V, E) :-
   element(T, V),
@@ -15,7 +13,6 @@ list2ens([T|Q], V, E) :-
 list2ens(L, E) :-
     list2ens(L, [], E), !.
 
-% renvoie si une liste est un ensemble
 ensemble(L) :-
     list2ens(L, L1),
     inv(L1, L2),
