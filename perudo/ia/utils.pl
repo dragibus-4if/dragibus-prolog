204916b99bb2d7adc3b36e4567e2b41c7c63f8b2
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-31 15:52:08 +0100

    Petite modif des utils

--- perudo/ia/utils.pl
+++ perudo/ia/utils.pl
@@ -1,33 +1,19 @@
 :- use_module(library(lists)).
+
 :- [coefBinomial].
 
+% Deuxième élément d'une paire
 pair_set_second(V, E, (E, V)).
 
-% Répéte un prédicat jusqu'à ce qu'il soit vrai
-repeter.
-repeter(P) :- P ; repeter(P).
-
-% compter le nombre d'occurences d'un élément dans une liste
+% Compter le nombre d'occurences d'un élément dans une liste
 compter([], _, 0).
 compter([X|T], X, Y):- compter(T, X, Z), Y is 1 + Z.
 compter([X1|T], X, Z):- X1 \= X, compter(T, X, Z).
 
-% Calcul du coefficient binomial C(n, p)
-% coefBinomial(_, 0, 1) :- !.
-% coefBinomial(N, N, 1) :- !.
-% coefBinomial(N, P, B) :-
-%   N1 is N-1,
-%   P1 is P-1,
-%   coefBinomial(N1, P1, B1),
-%   coefBinomial(N1, P, B2),
-%   B is B1 + B2, !.
-
-% Répartition binomiale
+% Calcul de la probabilité d'un coup, en considérant ses dés
 repartitionBinomiale(N, Q, P, Stat) :-
   coefBinomial(N, Q, C),
   Stat is C * (P)**Q * (1-P)**(N-Q), !.
-
-% Calcul de la probabilité d'un coup, en considérant ses dés
 statCoup(Des, NbTotal, rulesBet(Nb, De), Stat) :-
   % calculer le nombre de dés "manquants"
   compter(Des, De, NbMesDesCorrespondant),
