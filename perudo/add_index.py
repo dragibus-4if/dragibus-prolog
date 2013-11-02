8bf6081097c6102c1da7ead8caa9cf4e32e6c7c5
pierre.turpin@insa-lyon.fr <pierre.turpin@insa-lyon.fr>
2013-11-02 18:37:30 +0100

    Voila

--- perudo/add_index.py
+++
@@ -1,8 +0,0 @@
-#!/bin/env python2.7
-import sys
-line = sys.stdin.readline()
-t = 0
-while line:
-    print t, line.split('\n')[0]
-    t += 1
-    line = sys.stdin.readline()
--- perudo/low_pass_filter.py
+++ perudo/low_pass_filter.py
@@ -3,8 +3,11 @@ import sys
 size_filter = int(sys.argv[1])
 ls = [0.0] * size_filter
 line = sys.stdin.readline()
+t = 0
 while line:
     val = float(line.split('\n')[0])
     ls = ls[1:] + [val]
-    print sum(ls) / size_filter
+    t += 1
+    if t >= size_filter:
+        print t, sum(ls) / size_filter
     line = sys.stdin.readline()
