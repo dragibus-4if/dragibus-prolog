b291009a7748b6bb1b9f774aa5a9fa2309ab1f89
pierre.turpin@insa-lyon.fr <pierre.turpin@insa-lyon.fr>
2013-11-02 20:28:52 +0100

    ajout d'un plot par fichier

---
+++ perudo/plot_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+
+import sys
+import pylab
+
+if len(sys.argv) < 2:
+    sys.exit()
+
+for filename in sys.argv[1:]:
+    data, label = pylab.loadtxt(filename), filename
+    pylab.plot(data[:,0], data[:,1], label=label)
+
+pylab.legend()
+pylab.title("Title of Plot")
+pylab.xlabel("X Axis Label")
+pylab.ylabel("Y Axis Label")
+pylab.show()
