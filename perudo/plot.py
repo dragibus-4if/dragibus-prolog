520e17de9efb8b487e8728202572e61c4a89ec8a
pierre.turpin@insa-lyon.fr <pierre.turpin@insa-lyon.fr>
2013-11-02 18:40:29 +0100

    Label qui veut dire qqch

--- perudo/plot.py
+++ perudo/plot.py
@@ -3,7 +3,7 @@ import sys
 import pylab
 data = map(lambda x: [float(e) for e in x.split('\n')[0].split(' ')], sys.stdin.readlines())
 d = zip(*data)
-pylab.plot(d[0], d[1], label='Test')
+pylab.plot(d[0], d[1], label='Taux de victoire')
 pylab.legend()
 pylab.title("Title of Plot")
 pylab.xlabel("X Axis Label")
