6549ae625f575d6aa4810e9c3213f0d8feec7bf7
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-11-02 19:41:49 +0100

    Adding all versus matches

--- perudo/plot_matches.py
+++ perudo/plot_matches.py
@@ -9,12 +9,14 @@ ais = ['iaDebile', 'iaEleve', 'iaStats', 'iaIvre']
 # combinations for different matches
 nb_ais = len(ais)
 choices = [[int(i == j) for j in xrange(nb_ais)] for i in xrange(nb_ais)]
-combinations = list(it.combinations(choices, 2))
+ai_coefs = map(lambda x: x[0], it.combinations(choices, 1))
 
 # subprocess call
-nb_games = 100
-for a, b in combinations:
-    ratios = map(str, it.chain(a, b))
+nb_games = 10
+for coefs_a, coefs_b in ((a, b) for a in ai_coefs for b in ai_coefs):
+    ratios = it.imap(str, it.chain(coefs_a, coefs_b))
     sp_args = 'swipl -q -s game -t'.split()
     sp_args.append('go(%s, %s)' % (', '.join(ratios), nb_games))
-    print sp.check_output(sp_args, stderr=sp.PIPE)
+    print sp_args
+    lines = filter(None, sp.check_output(sp_args, stderr=sp.PIPE).split('\n'))
+    print lines
