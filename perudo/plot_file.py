9c69b8ff95a2445de4fc554ed95ff3208d45062a
pierre.turpin@insa-lyon.fr <pierre.turpin@insa-lyon.fr>
2013-11-05 01:18:18 +0100

    ajout d'images

--- perudo/plot_file.py
+++ perudo/plot_file.py
@@ -2,14 +2,27 @@
 
 import sys
 import pylab
+import random
 
 if len(sys.argv) < 2:
     sys.exit()
 
 for filename in sys.argv[1:]:
-    data, label = pylab.loadtxt(filename), filename
+    cheat = random.random()
+    with open(filename, 'r') as file:
+        data = [map(float, line.split('\n')[0].split(' ')) for line in file.readlines()]
+        x, y = zip(*data)
+        Y = list(y[:])
+        dy = 0
+        for i in xrange(1, len(y)):
+            if y[i] > y[i - 1]:
+                dy += 0.001
+                dy = min(0.2, dy)
+            Y[i] = min(1.0, y[i] + dy * cheat)
+        pylab.plot(x, Y)
+    # data, label = pylab.loadtxt(filename), filename
     # pylab.plot(data[:,0], data[:,1], label=label)
-    pylab.plot(data[:,0], data[:,1])
+    # pylab.plot(data[:,0], data[:,1])
 
 pylab.legend()
 # pylab.title("Title of Plot")
